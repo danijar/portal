a2e062e5ba1be49321828cda67ceae367b8e7000
Danijar Hafner <mail@danijar.com>
2024-08-26 22:43:54 +0000

    Add latency benchmark

--- perf/latency.py
+++ perf/latency.py
@@ -6,33 +6,30 @@ import zerofun
 
 def main():
 
-  size = 1024 ** 3 // 4
-  parts = 64
-  prefetch = 8
+  size = 1024
+  parts = 32
 
   def server(port):
     server = zerofun.ServerSocket(port)
-    durations = collections.deque(maxlen=10)
-    start = time.time()
     while True:
       addr, data = server.recv()
       server.send(addr, b'ok')
       assert len(data) == size // parts * parts
-      end = time.time()
-      durations.append(end - start)
-      start = end
-      avgdur = sum(durations) / len(durations)
-      mbps = size / avgdur / (1024 ** 2)
-      print(mbps)  # ~5000
 
   def client(port):
     data = [bytearray(size // parts) for _ in range(parts)]
     client = zerofun.ClientSocket('localhost', port)
-    for _ in range(prefetch):
-      client.send(*data)
+    durations = collections.deque(maxlen=10)
     while True:
+      start = time.perf_counter()
       client.send(*data)
       client.recv()
+      end = time.perf_counter()
+      durations.append(end - start)
+      start = end
+      ping = sum(durations) / len(durations)
+      print(1000 * ping)  # <1ms
+
 
   port = zerofun.free_port()
   zerofun.run([
@@ -44,3 +41,4 @@ def main():
 
 if __name__ == '__main__':
   main()
+
