446b4c76703a8cc6c5648e94bf0bec64302a32c7
Danijar Hafner <mail@danijar.com>
2024-09-02 12:47:55 -0700

    Fix errfile test

--- tests/test_errfile.py
+++ tests/test_errfile.py
@@ -11,31 +11,37 @@ class TestErrfile:
 
   def test_file(self, tmpdir):
     errfile = pathlib.Path(tmpdir) / 'error'
-    zerofun.setup(errfile, check_interval=0.1)
-    try:
-      raise ValueError
-    except Exception as e:
-      zerofun.context().error(e, 'worker')
+
+    def fn():
+      zerofun.setup(errfile, check_interval=0.1)
+      try:
+        raise ValueError
+      except Exception as e:
+        zerofun.context().error(e, 'worker')
+
+    zerofun.Process(fn, start=True).join()
     content = errfile.read_text()
     assert "Error in 'worker' (ValueError):" == content.split('\n')[0]
     assert 'Traceback (most recent call last)' in content
     assert 'line' in content
-    assert 'in test_file' in content
-    zerofun.close()
+    assert 'in fn' in content
 
   @pytest.mark.parametrize('repeat', range(3))
   def test_sibling_procs(self, tmpdir, repeat):
     errfile = pathlib.Path(tmpdir) / 'error'
     ready = zerofun.context().mp.Semaphore(0)
+
     def fn1(ready, errfile):
       zerofun.setup(errfile, check_interval=0.1)
       ready.release()
       raise ValueError('payload')
+
     def fn2(ready, errfile):
       zerofun.setup(errfile, check_interval=0.1)
       ready.release()
       while True:
         time.sleep(0.1)
+
     worker1 = zerofun.Process(fn1, ready, errfile, name='worker1', start=True)
     worker2 = zerofun.Process(fn2, ready, errfile, name='worker2', start=True)
     ready.acquire()
@@ -49,15 +55,16 @@ class TestErrfile:
     assert not worker2.running
     assert worker1.exitcode == 1
     assert worker2.exitcode == 2
-    zerofun.close()
 
   @pytest.mark.parametrize('repeat', range(3))
   def test_nested_procs(self, tmpdir, repeat):
     errfile = pathlib.Path(tmpdir) / 'error'
     ready = zerofun.context().mp.Semaphore(0)
+
     def hang():
       while True:
         time.sleep(0.1)
+
     def outer(ready, errfile):
       zerofun.setup(errfile, check_interval=0.1)
       zerofun.Process(inner, ready, errfile, name='inner', start=True)
@@ -65,12 +72,14 @@ class TestErrfile:
       zerofun.Process(hang, start=True)
       ready.release()
       hang()
+
     def inner(ready, errfile):
       zerofun.setup(errfile, check_interval=0.1)
       zerofun.Thread(hang, start=True)
       zerofun.Process(hang, start=True)
       ready.release()
       raise ValueError('payload')
+
     worker = zerofun.Process(outer, ready, errfile, name='outer', start=True)
     ready.acquire()
     ready.acquire()
@@ -78,4 +87,3 @@ class TestErrfile:
     content = errfile.read_text()
     assert "Error in 'inner' (ValueError: payload):" == content.split('\n')[0]
     assert not worker.running
-    zerofun.close()
